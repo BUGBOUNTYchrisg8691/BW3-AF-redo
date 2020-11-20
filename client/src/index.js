b0e21c0812316382fbdbca91f9bd81179f224591
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-20 16:40:07 -0500

    init commit

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+function App() {
+  return (
+    <div className="app-container">
+      App
+      {/* this is a test */}
+    </div>
+  );
+}
