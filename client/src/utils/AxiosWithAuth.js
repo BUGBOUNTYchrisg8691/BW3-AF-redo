05171a769e594ad3063e23ff628161dc10b9b6c7
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-21 14:13:24 -0500

    fucking cors; i wish people would do their jobs correctly

--- client/src/utils/AxiosWithAuth.js
+++ client/src/utils/AxiosWithAuth.js
@@ -7,8 +7,8 @@ export default function AxiosWithAuth() {
   const token = JSON.parse(localStorage.getItem("user")).token;
   return Axios.create({
     // baseURL: LOCAL,
-    // baseURL: `https://cors-anywhere.herokuapp.com/${BASE_URL}`,
-    baseURL: BASE_URL,
+    baseURL: `https://cors-anywhere.herokuapp.com/${BASE_URL}`,
+    // baseURL: BASE_URL,
     headers: {
       Authorization: `${token}`,
       "Access-Control-Allow-Origin": "*",
